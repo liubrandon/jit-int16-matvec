020f10b26c3945016e9240a01b56e00c0fa5f2a6
Brandon Liu <byliu88@gmail.com>
2020-07-16 19:22:12 -0400

    16x16 instruction comparison statistics added

---
+++ asm_to_xbyak.py
@@ -0,0 +1,11 @@
+import re
+# Takes in a .asm file from Zydis output and reformats the assembly for Xbyak
+f = open("mkl64x16.asm", "r")
+text = f.read()
+for line in text.splitlines():
+    noAddr = re.sub(r'0x\w+\s+', '', line)
+    withParens = noAddr.replace(' ', '(', 1) + ');'
+    output = re.sub(r'(\w)(mm)?word ptr', r'\1word', withParens)
+    if output.count('(') == 0:
+        output = output.replace(');', '();', 1)
+    print(output)
