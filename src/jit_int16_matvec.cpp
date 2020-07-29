de0970d09379fe308c77ca340e22a2b9e2729ff8
Brandon Liu <byliu88@gmail.com>
2020-07-29 16:08:10 -0400

    Up o 128xK where M and K are multiples of 16 works

--- src/jit_int16_matvec.cpp
+++ src/jit_int16_matvec.cpp
@@ -9,6 +9,8 @@
 #include "timer.hpp"
 #include <iomanip>
 #include <vector>
+#include <assert.h>
+#include <numeric>
 #define DIE(...) fprintf(stderr, __VA_ARGS__); exit(1);
 struct Complex_float {
     float real;
@@ -37,7 +39,7 @@ struct Complex_int16 {
         return os;
     }
 };
-void outputASM(void* func, int m, int n, std::string filePrefix) {
+void outputASM(void* func, int m, int n, std::string filePrefix, std::string type) {
     // Initialize decoder context
     ZydisDecoder decoder;
     ZydisDecoderInit(&decoder, ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_ADDRESS_WIDTH_64);
@@ -57,7 +59,7 @@ void outputASM(void* func, int m, int n, std::string filePrefix) {
 
     // File to output ASM
     std::ofstream outFile;
-    std::string filename = filePrefix + std::to_string(m) + "x" + std::to_string(n) + ".asm";
+    std::string filename = filePrefix + std::to_string(m) + "x" + std::to_string(n) + type + ".asm";
     outFile.open(filename);
     while (ZYAN_SUCCESS(ZydisDecoderDecodeBuffer(&decoder, (void*)((uintptr_t)func + offset), length - offset, &instruction))) {
         // Print current instruction pointer.
@@ -85,7 +87,7 @@ __m512i swapPairs = _mm512_loadu_si512((const void*)temp);
 __m512i subAdd = _mm512_loadu_si512((const void*)temp1);
 struct JitInt16MatVec : Xbyak::CodeGenerator {
     JitInt16MatVec(int m, int k)
-        : Xbyak::CodeGenerator(50*4096, Xbyak::DontSetProtectRWE) // Use Read/Exec mode for security
+        : Xbyak::CodeGenerator(100*4096, Xbyak::DontSetProtectRWE) // Use Read/Exec mode for security
     {  // Input parameters rdi=&broad16, rsi=mat, rdx=vec, rcx=res, r8=&swapPairs (https://aaronbloomfield.github.io/pdr/book/x86-64bit-ccc-chapter.pdf)
         sub(rsp, 0x04*k); // allocate 256 bytes to the stack (size of 64 Complex_int16)
         vpbroadcastd(zmm31, dword [rdi]); //  rdi = {1, -1, ...}
@@ -119,6 +121,26 @@ struct JitInt16MatVec : Xbyak::CodeGenerator {
             vpmaddwd(zmm23, zmm30, zmm5);
             vpmaddwd(zmm22, zmm30, zmm6);
         }        
+        if (m >= 80) {
+            vmovdqu16(zmm30, zword [rsi+0x100]);
+            vpmaddwd(zmm21, zmm30, zmm5);
+            vpmaddwd(zmm20, zmm30, zmm6);
+        }
+        if (m >= 96) {
+            vmovdqu16(zmm30, zword [rsi+0x140]);
+            vpmaddwd(zmm19, zmm30, zmm5);
+            vpmaddwd(zmm18, zmm30, zmm6);
+        }
+        if (m >= 112) {
+            vmovdqu16(zmm30, zword [rsi+0x180]);
+            vpmaddwd(zmm17, zmm30, zmm5);
+            vpmaddwd(zmm16, zmm30, zmm6);
+        }        
+        if (m >= 128) {
+            vmovdqu16(zmm30, zword [rsi+0x1C0]);
+            vpmaddwd(zmm15, zmm30, zmm5);
+            vpmaddwd(zmm14, zmm30, zmm6);
+        }        
         int rowsize = m*4;
         for(int i = 1; i < k; i++) {
             vmovdqu16(zmm30, zword [rsi+(rowsize*i)]);
@@ -141,6 +163,26 @@ struct JitInt16MatVec : Xbyak::CodeGenerator {
                 vmovdqu16(zmm30, zword [rsi+(rowsize*i)+0xC0]);
                 vpdpwssds(zmm23, zmm30, zmm5);
                 vpdpwssds(zmm22, zmm30, zmm6);
+            }
+            if (m >= 80) {
+                vmovdqu16(zmm30, zword [rsi+(rowsize*i)+0x100]);
+                vpdpwssds(zmm21, zmm30, zmm5);
+                vpdpwssds(zmm20, zmm30, zmm6);
+            } 
+            if (m >= 96) {
+                vmovdqu16(zmm30, zword [rsi+(rowsize*i)+0x140]);
+                vpdpwssds(zmm19, zmm30, zmm5);
+                vpdpwssds(zmm18, zmm30, zmm6);
+            } 
+            if (m >= 112) {
+                vmovdqu16(zmm30, zword [rsi+(rowsize*i)+0x180]);
+                vpdpwssds(zmm17, zmm30, zmm5);
+                vpdpwssds(zmm16, zmm30, zmm6);
+            }
+            if (m >= 128) {
+                vmovdqu16(zmm30, zword [rsi+(rowsize*i)+0x1C0]);
+                vpdpwssds(zmm15, zmm30, zmm5);
+                vpdpwssds(zmm14, zmm30, zmm6);
             }
         }
         add(rsp, 0x04*k);
@@ -168,72 +210,88 @@ struct JitInt16MatVec : Xbyak::CodeGenerator {
             vmovdqu16(zmm23 | k1, zmm22);
             vmovdqa64(zword [rcx+0xC0], zmm23);
         }
+        if (m >= 80) {
+            vpslld(zmm20, zmm20, 0x10); // shift imag_res 16 bits left
+            vmovdqu16(zmm21 | k1, zmm20);
+            vmovdqa64(zword [rcx+0x100], zmm21);
+        }
+        if (m >= 96) {
+            vpslld(zmm18, zmm18, 0x10); // shift imag_res 16 bits left
+            vmovdqu16(zmm19 | k1, zmm18);
+            vmovdqa64(zword [rcx+0x140], zmm19);
+        }
+        if (m >= 112) {
+            vpslld(zmm16, zmm16, 0x10); // shift imag_res 16 bits left
+            vmovdqu16(zmm17 | k1, zmm16);
+            vmovdqa64(zword [rcx+0x180], zmm17);
+        }
+        if (m >= 128) {
+            vpslld(zmm14, zmm14, 0x10); // shift imag_res 16 bits left
+            vmovdqu16(zmm15 | k1, zmm14);
+            vmovdqa64(zword [rcx+0x1C0], zmm15);
+        }
         ret();
     }
 };
 
 double runJITCGEMM(MKL_Complex8* a, MKL_Complex8* b, MKL_Complex8* c, MKL_INT m, MKL_INT k, int numIter) {
+    if(numIter == 0) return 0.0;
     MKL_Complex8 alpha = {1, 0};
     MKL_Complex8 beta = {0, 0};
     MKL_INT lda = m;
     MKL_INT ldb = k;
     MKL_INT ldc = m;
-    void* jitter;
     double start = getTime();
+    // Create a handle and generate GEMM kernel
+    void* jitter;
     mkl_jit_status_t status = mkl_jit_create_cgemm(&jitter, MKL_COL_MAJOR, MKL_NOTRANS, MKL_NOTRANS, m, 1, k, &alpha, lda, ldb, &beta, ldc);
-    // if (MKL_JIT_ERROR == status) {
-    //     fprintf(stderr, "Error: insufficient memory to JIT and store the DGEMM kernel\n");
-    //     exit(1);
-    // }
+    if (MKL_JIT_ERROR == status) {
+        fprintf(stderr, "Error: insufficient memory to JIT and store the DGEMM kernel\n");
+        exit(1);
+    }
+    // Get kernel associated with handle
     cgemm_jit_kernel_t my_cgemm = mkl_jit_get_cgemm_ptr(jitter);
     for(int i = 0; i < numIter; i++)
-        my_cgemm(jitter, a, b, c); // execute the GEMM kernel
-    double ret = timeSince(start);
-    outputASM((void*)my_cgemm, m, k, "/asm/mkl");
+        my_cgemm(jitter, a, b, c); // Repeatedly execute the GEMM kernel
+    // Destroy the created handle/GEMM kernel
     mkl_jit_destroy((void*)my_cgemm);
+    double ret = timeSince(start);
+    outputASM((void*)my_cgemm, m, k, std::string("./asm/") + std::to_string(m) + "xK/", "_mkl");
     return ret;
 }
 
 std::vector<std::string> dimensions;
-std::vector<double> armaTimes, cgemvTimes, cgemmTimes, jitcgemmTimes, cgemvTimes_row, jitcgemmTimes_row;
-void outputCSV() {
+std::vector<double> mklTimes, myTimes;
+void outputCSV(std::string filename) {
     std::ofstream outFile;
-    outFile.open("results.csv");
-    outFile << "Dimensions,Armadillo cgemv,cgemv,cgemv row major,cgemm,JIT cgemm,JIT cgemm row major\n";
+    outFile.open(filename+".csv");
+    outFile << "Dimensions,MKL,My int16,Speedup,";
+    outFile << "Overall avg. speedup:," << std::accumulate(mklTimes.begin(), mklTimes.end(), 0.0)/(double)std::accumulate(myTimes.begin(), myTimes.end(), 0.0) << ",\n";
     for(int i = 0; i < dimensions.size(); i++)
-        outFile << dimensions[i] << "," << armaTimes[i] << "," << cgemvTimes[i] << "," << cgemvTimes_row[i] << "," << cgemmTimes[i] << "," << jitcgemmTimes[i] << "," << jitcgemmTimes_row[i] << ",\n";
+        outFile << dimensions[i] << "," << mklTimes[i] << "," << myTimes[i] << "," << mklTimes[i]/myTimes[i] << ",\n";
     outFile.close();
+    dimensions.clear(); mklTimes.clear(); myTimes.clear();
 }
 
-int main(int argc, char** argv) {
-    // Parse arguments and declare/initialize variables
-    if(argc < 2) {
-        DIE("Usage: %s MxK where M is # rows and K is # cols\n", argv[0]);
+bool vectorsEqual(float* vec1, int16_t* vec2, long size) {
+    for(int i = 0; i < size*2; i++) {
+        if((int16_t)vec1[i] != vec2[i]) return false;
     }
-    srand(time(0));
-    long m, k, numIter;
-    Complex_float *mat, *vec, *res, *res1, *res2;
+    return true;
+}
+
+void benchDimensions(long m, long k, long numIter) {
+    MKL_Complex8 *mat, *vec, *res;
     Complex_int16 *mat16, *vec16, *res16;
-    char* nPtr;
-    m = strtoul(argv[1], &nPtr, 0);
-    k = strtoul(nPtr+1, NULL, 0);
-    numIter = (argc >= 3) ? strtoul(argv[2], NULL, 0) : 1000000;
     // Allocate memory for matrix, vector, and resulting vector aligned on 64 byte boundary
-    mat = (Complex_float*)aligned_alloc(64, m*k*sizeof(Complex_float));
-    vec = (Complex_float*)aligned_alloc(64, k*sizeof(Complex_float));
-    res = (Complex_float*)aligned_alloc(64, m*sizeof(Complex_float));
-    memset(res, 0, m*sizeof(Complex_float));
-    res1 = (Complex_float*)aligned_alloc(64, m*sizeof(Complex_float));
-    memset(res1, 0, m*sizeof(Complex_float));
-    res2 = (Complex_float*)aligned_alloc(64, m*sizeof(Complex_float));
-    memset(res2, 0, m*sizeof(Complex_float));
-    
+    mat = (MKL_Complex8*)mkl_calloc(m*k, sizeof(MKL_Complex8), 64);
+    vec = (MKL_Complex8*)mkl_calloc(k, sizeof(MKL_Complex8), 64);
+    res = (MKL_Complex8*)mkl_calloc(m, sizeof(MKL_Complex8), 64);
     // Int16 version
     mat16 = (Complex_int16*)aligned_alloc(64, m*k*sizeof(Complex_int16));
     vec16 = (Complex_int16*)aligned_alloc(64, k*sizeof(Complex_int16));
     res16 = (Complex_int16*)aligned_alloc(64, m*sizeof(Complex_int16));
     memset(res16, 0, m*sizeof(Complex_int16));
-
     // Randomly generate matrix/vector with values from -range to range
     int mod = 10; //rand()%mod-range
     int range = mod/2;
@@ -245,63 +303,46 @@ int main(int argc, char** argv) {
         vec16[i] = {(int16_t)(rand()%mod-range), (int16_t)(rand()%mod-range)};
         vec[i] = {(float)vec16[i].real, (float)vec16[i].imag};
     }
-    // for(int i = 0; i < k; i++) {
-    //     for(int j = 0; j < m; j++)
-    //         std::cout << mat16[i*m+j];
-    //     std::cout << std::endl;
-    // }
-    // std::cout << std::endl;
-    // for(int i = 0; i < k; i++) std::cout << vec16[i];
-    // std::cout << std::endl;
-
     double start = getTime();
-    // JitFloatMatVec jit(m, k);
-    // jit.setProtectModeRE(); // Use Read/Exec mode for security
-    // void (*matvec)(void* notUsed, const Complex_float*, const Complex_float*, Complex_float*) = jit.getCode<void (*)(void* notUsed, const Complex_float*, const Complex_float*, Complex_float*)>();
-    // for(int i = 0; i < numIter; i++) {
-    //     matvec((void*)&broad, mat, vec, res);
-    // }
-    double myFloatTime = timeSince(start);
-    start = getTime();
-    // for(int i = 0; i < numIter; i++)
-    //     matvecFloat_64x16((MKL_Complex8*)mat, (MKL_Complex8*)vec, (MKL_Complex8*)res2);
-    double oldFloatTime = timeSince(start);
-    start = getTime();
     JitInt16MatVec jit16(m, k);
     jit16.setProtectModeRE(); // Use Read/Exec mode for security
     void (*matvec16)(void* broad, const Complex_int16*, const Complex_int16*, Complex_int16*, void* swapPairs) = jit16.getCode<void (*)(void* broad, const Complex_int16*, const Complex_int16*, Complex_int16*, void* swapPairs)>();
     for(int i = 0; i < numIter; i++)
         matvec16((void*)&broad16, mat16, vec16, res16, (void*)&swapPairs);
     double myTime = timeSince(start);
-
     // Generate code at runtime (Just-in-Time) and output asm
-    double mklTime = runJITCGEMM((MKL_Complex8*)mat, (MKL_Complex8*)vec, (MKL_Complex8*)res1, m, k, numIter);
-
-    // Save .asm of each function
-    //outputASM((void*)matvec, m, k, "./asm/myfloat");
-    //outputASM((void*)matvec16, m, k, "./asm/myint16");
-
+    double mklTime = runJITCGEMM(mat, vec, res, m, k, numIter);
+    // Save .asm of my function (MKL .asm is saved in runJITCGEMM)
+    outputASM((void*)matvec16, m, k, std::string("./asm/") + std::to_string(m) + std::string("xK/"), "_myint16");
     // Output result
-    // for(int i = 0; i < m; i++) std::cout << res[i];
+    // for(int i = 0; i < m; i++) std::cout << "(" << std::setprecision(0) << res[i].real << "," << std::setprecision(0) << res[i].imag << ")";
     // std::cout << std::endl;
-    for(int i = 0; i < m; i++) std::cout << res1[i];
-    std::cout << std::endl;
-    // for(int i = 0; i < m; i++) std::cout << res2[i];
+    // for(int i = 0; i < m; i++) std::cout << res16[i];
     // std::cout << std::endl;
-    for(int i = 0; i < m; i++) std::cout << res16[i];
-    std::cout << std::endl;
-
-
     printf("\n        ---------- \n\n");
     printf("     %ld iterations, (%ldx%ld) * (%ldx%d)\n", numIter, m, k, k, 1);
     printf("MKL JIT cgemm: %.10f µs per iteration\n", mklTime/(double)numIter);
-    // printf(" my JIT float: %.10f µs per iteration\n", myFloatTime/(double)numIter);
-    // printf("    old float: %.10f µs per iteration\n", oldFloatTime/(double)numIter);
     printf(" my JIT int16: %.10f µs per iteration\n", myTime/(double)numIter);
     #define RESET   "\033[0m" // Terminal color codes
     #define BOLDGREEN   "\033[1m\033[32m" 
     std::cout << "  " << BOLDGREEN << std::fixed << std::setprecision(2) << mklTime/myTime << "x" << RESET << " MKL JIT cgemm" << std::endl;
-    // // Free allocated memory
-    // free(mat); free(vec); free(res); free(res1); free(res2);
-    // free(mat16); free(vec16); free(res16);
+    std::cout << "---------------------------------\n" << std::endl;
+    // Assert resulting values are equal
+    assert(vectorsEqual((float*)res, (int16_t*)res16, m));
+    dimensions.push_back(std::to_string(m) + "x" + std::to_string(k));
+    mklTimes.push_back(mklTime/(double)numIter);
+    myTimes.push_back(myTime/(double)numIter);
+    // Free allocated memory
+    mkl_free(mat); mkl_free(vec); mkl_free(res);
+    free(mat16); free(vec16); free(res16);
+}
+
+int main(int argc, char** argv) {
+    srand(time(0));
+    long numIter = 100000;
+    for(long m = 128; m <= 128; m+=16) {
+        for(long k = 16; k <= 1024; k += 16)
+            benchDimensions(m, k, numIter);    
+        outputCSV(std::to_string(m));
+    }
 }
