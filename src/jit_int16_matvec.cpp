c4678a687d492344793f128cdfd437b5e8118b5c
Brandon Liu <byliu88@gmail.com>
2020-08-24 15:39:54 -0400

    64x16 fixed poin within 0.05

--- src/jit_int16_matvec.cpp
+++ src/jit_int16_matvec.cpp
@@ -14,7 +14,8 @@
 #include <math.h>
 #include <random>
 #define DIE(...) fprintf(stderr, __VA_ARGS__); exit(1);
-#define FIXED_POINT_FRACTIONAL_BITS 3
+#define FIXED_POINT_FRACTIONAL_BITS 9
+#define TOLERANCE 0.05f
 
 inline int16_t floatToFixed(float flo) {
     return (int16_t)(round(flo * (1 << FIXED_POINT_FRACTIONAL_BITS)));
@@ -154,13 +155,17 @@ struct JitInt16MatVec : Xbyak::CodeGenerator {
             vpaddd(zmm22,zmm22,zmm2);
             vpaddd(zmm28,zmm28,zmm4);
             vpaddd(zmm28,zmm28,zmm22);
+            // Shift out exccess fractional bits to correct scaling factor
             vpsrad(zmm28, zmm28, FIXED_POINT_FRACTIONAL_BITS);
             // Sum up accumulators for real results
             vpaddd(zmm29,zmm29,zmm3);
             vpaddd(zmm21,zmm21,zmm1);
             vpaddd(zmm29,zmm29,zmm21);
+            // Correct scaling factor
             vpsrad(zmm29, zmm29, FIXED_POINT_FRACTIONAL_BITS);
+            // Downcast from 32 bits to 16 bits and keep sign, packing both results into zmm29
             vpackssdw(zmm29, zmm29, zmm28);
+            // Correct order of the final result
             vpshufb(zmm29, zmm29, zmm0);
             
             // vpslld(zmm28, zmm28, 0x10); // shift imag_res 16 bits left
@@ -313,27 +318,40 @@ struct JitInt16MatVec : Xbyak::CodeGenerator {
                         vpdpwssds(zmm2 , zmm30, zmm6);
                     }
                 }
-                vpslld(zmm28, zmm28, 0x10); // shift imag_res 16 bits left
-                // Set up writemask k1
-                mov(esi, 0xAAAAAAAA);
-                kmovd(k1, esi);
-                // Interleave real and imaginary
-                vmovdqu16(zmm29 | k1, zmm28);
+                vmovdqu16(zmm0, zword [r9]);      // zmm0 = swapAfterPack
+                // vpslld(zmm28, zmm28, 0x10); // shift imag_res 16 bits left
+                // // Set up writemask k1
+                // mov(esi, 0xAAAAAAAA);
+                // kmovd(k1, esi);
+                // // Interleave real and imaginary
+                // vmovdqu16(zmm29 | k1, zmm28);
+                vpsrad(zmm28, zmm28, FIXED_POINT_FRACTIONAL_BITS);
+                vpsrad(zmm29, zmm29, FIXED_POINT_FRACTIONAL_BITS);
+                // Downcast from 32 bits to 16 bits and keep sign, packing both results into zmm29
+                vpackssdw(zmm29, zmm29, zmm28);
+                // Correct order of the final result
+                vpshufb(zmm29, zmm29, zmm0);
                 // Write to memory
                 vmovdqa64(zword [rcx+(r*0x04)], zmm29);
                 if (m-r >= 32) {
-                    vpslld(zmm26, zmm26, 0x10);
-                    vmovdqu16(zmm27 | k1, zmm26);
+                    vpsrad(zmm27, zmm27, FIXED_POINT_FRACTIONAL_BITS);
+                    vpsrad(zmm26, zmm26, FIXED_POINT_FRACTIONAL_BITS);
+                    vpackssdw(zmm27, zmm27, zmm26);
+                    vpshufb(zmm27, zmm27, zmm0);
                     vmovdqa64(zword [rcx+(r*0x04)+0x40], zmm27);
                 }
                 if (m-r >= 48) {
-                    vpslld(zmm24, zmm24, 0x10);
-                    vmovdqu16(zmm25 | k1, zmm24);
+                    vpsrad(zmm25, zmm25, FIXED_POINT_FRACTIONAL_BITS);
+                    vpsrad(zmm24, zmm24, FIXED_POINT_FRACTIONAL_BITS);
+                    vpackssdw(zmm25, zmm25, zmm24);
+                    vpshufb(zmm25, zmm25, zmm0);
                     vmovdqa64(zword [rcx+(r*0x04)+0x80], zmm25);
                 }
                 if (m-r >= 64) {
-                    vpslld(zmm22, zmm22, 0x10);
-                    vmovdqu16(zmm23 | k1, zmm22);
+                    vpsrad(zmm23, zmm23, FIXED_POINT_FRACTIONAL_BITS);
+                    vpsrad(zmm22, zmm22, FIXED_POINT_FRACTIONAL_BITS);
+                    vpackssdw(zmm23, zmm23, zmm22);
+                    vpshufb(zmm23, zmm23, zmm0);
                     vmovdqa64(zword [rcx+(r*0x04)+0xC0], zmm23);
                 }
                 if (m-r >= 80) {
@@ -428,13 +446,18 @@ void outputCSV(std::string filename) {
     dimensions.clear(); mklTimes.clear(); myTimes.clear();
 }
 
-bool vectorsEqual(float* vec1, int16_t* vec2, long size) {
-    for(int i = 0; i < size*2; i++) {
-        if((int16_t)vec1[i] != vec2[i])
+bool cmpf(MKL_Complex8 a, Complex_int16 b, float epsilon = TOLERANCE) {
+    return (fabs(a.real - fixedToFloat(b.real)) < epsilon) && (fabs(a.imag - fixedToFloat(b.imag)) < epsilon);
+}
+
+bool vectorsEqual(MKL_Complex8* src, Complex_int16* mine, long m) {
+    for(int i = 0; i < m; i++) {
+        if(!(cmpf(src[i], mine[i])))
             return false;
     }
     return true;
 }
+
 enum PROGRAM_MODE {
     MKL,
     MINE,
@@ -455,12 +478,12 @@ void benchDimensions(long m, long k, long numIter, PROGRAM_MODE mode, bool real)
     // Randomly generate matrix/vector with values from 0 to 50
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis(-20.0, 20.0);
+    std::uniform_real_distribution<float> mat_dis(-1.0, 1.0);
     int mod = 10; //rand()%mod-range
     int range = mod/2;
     for(int i = 0; i < m*k; i++) {
         if(real) {
-            mat[i] = {dis(gen), dis(gen)};
+            mat[i] = {mat_dis(gen), mat_dis(gen)};
             mat16[i] = {floatToFixed(mat[i].real), floatToFixed(mat[i].imag)};
         } else {
             mat16[i] = {(int16_t)(rand()%mod-range), (int16_t)(rand()%mod-range)};
@@ -470,9 +493,10 @@ void benchDimensions(long m, long k, long numIter, PROGRAM_MODE mode, bool real)
         // std::cout << "(" << fixedToFloat(mat16[i].real) << "," << fixedToFloat(mat16[i].imag) << ")" << std::endl << std::endl;
 
     }
+    std::uniform_real_distribution<float> vec_dis(-5.0, 5.0);
     for(int i = 0; i < k; i++) {
         if(real) {
-            vec[i] = {dis(gen), dis(gen)};
+            vec[i] = {vec_dis(gen), vec_dis(gen)};
             vec16[i] = {floatToFixed(vec[i].real), floatToFixed(vec[i].imag)};
         } else {
             vec16[i] = {(int16_t)(rand()%mod-range), (int16_t)(rand()%mod-range)};
@@ -500,11 +524,11 @@ void benchDimensions(long m, long k, long numIter, PROGRAM_MODE mode, bool real)
     }
     // Save .asm of my function (MKL .asm is saved in runJITCGEMM)
     // Output result
-    for(int i = 0; i < m; i++) std::cout << "(" << res[i].real << "," << res[i].imag << ")";
+    for(int i = 0; i < m; i++) std::cout << "(" << std::fixed << std::setprecision(2) << res[i].real << "," << std::fixed << std::setprecision(2) << res[i].imag << ")";
     std::cout << std::endl;
     for(int i = 0; i < m; i++) {
         if(real)
-            std::cout << "(" << fixedToFloat(res16[i].real) << "," << fixedToFloat(res16[i].imag) << ")";
+            std::cout << "(" << std::fixed << std::setprecision(2) << fixedToFloat(res16[i].real) << "," << std::fixed << std::setprecision(2) << fixedToFloat(res16[i].imag) << ")";
         else 
             std::cout << res16[i];
     }
@@ -518,7 +542,7 @@ void benchDimensions(long m, long k, long numIter, PROGRAM_MODE mode, bool real)
     std::cout << "  " << BOLDGREEN << std::fixed << std::setprecision(2) << mklTime/myTime << "x" << RESET << " MKL JIT cgemm" << std::endl;
     std::cout << "---------------------------------\n" << std::endl;
     // Assert resulting values are equal
-    if(mode == BOTH) assert(vectorsEqual((float*)res, (int16_t*)res16, m));
+    if(mode == BOTH) assert(vectorsEqual(res, res16, m));
     dimensions.push_back(std::to_string(m) + "x" + std::to_string(k));
     mklTimes.push_back(mklTime/(double)numIter);
     myTimes.push_back(myTime/(double)numIter);
